c27b04cd2b7c2c62264b35877c323dc1e572f607
Superrusp <amir.dogmosh@gmail.com>
2021-06-06 19:19:16 +0200

    add validation of requests for not blank values

--- backend/src/main/java/com/kpi/springlabs/backend/model/dto/request/AuthenticationRequest.java
+++ backend/src/main/java/com/kpi/springlabs/backend/model/dto/request/AuthenticationRequest.java
@@ -4,20 +4,20 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Data
 @ApiModel(value = "Authentication request",
-        description = "The response body in JSON format is used to authenticate the user.")
+        description = "The request body in JSON format is used to authenticate the user.")
 public class AuthenticationRequest {
 
-    @NotNull(message = "Username cannot be null.")
+    @NotBlank(message = "Username cannot be blank.")
     @Size(min = 2, max = 32, message = "The username must be from 2 to 32 characters long.")
     @ApiModelProperty(value = "Username", position = 1, required = true)
     private String username;
 
-    @NotNull(message = "Password cannot be null.")
+    @NotBlank(message = "Password cannot be blank.")
     @Size(min = 5, max = 50, message = "The password must be from 5 to 50 characters long.")
     @ApiModelProperty(value = "Password", position = 2, example = "12345", required = true)
     private String password;
--- backend/src/main/java/com/kpi/springlabs/backend/model/dto/request/RegistrationRequest.java
+++ backend/src/main/java/com/kpi/springlabs/backend/model/dto/request/RegistrationRequest.java
@@ -5,15 +5,15 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Data
 @ApiModel(value = "Registration request",
-        description = "The response body in JSON format is used to register a new user.")
+        description = "The request body in JSON format is used to register a new user.")
 public class RegistrationRequest {
 
-    @NotNull(message = "Username cannot be null.")
+    @NotBlank(message = "Username cannot be blank.")
     @Size(min = 2, max = 32, message = "The username must be from 2 to 32 characters long.")
     @ApiModelProperty(value = "Username", position = 1, example = "tester1", required = true)
     private String username;
@@ -23,7 +23,7 @@ public class RegistrationRequest {
     @ApiModelProperty(value = "Email for registration", position = 2, example = "name@test.com", required = true)
     private String email;
 
-    @NotNull(message = "Password cannot be null.")
+    @NotBlank(message = "Password cannot be blank.")
     @Size(min = 5, max = 50, message = "The password must be from 5 to 50 characters long.")
     @ApiModelProperty(value = "Password", position = 3, example = "12345", required = true)
     private String password;
